c3abf06ce5b0c57eaec8c7d515a059f13259b7f3
Dominic Chapman <dominicchapman@me.com>
2017-10-10 16:22:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,8 +24,7 @@ class CashRegister
   # CashRegister #apply_discount the cash register was initialized with an employee discount applies the discount to the total price
   # CashRegister #apply_discount the cash register was initialized with an employee discount returns success message with updated total
   # CashRegister #apply_discount the cash register was initialized with an employee discount reduces the total
-  # CashRegister #apply_discount the cash register was not initialized with an employee discount returns a string error message that
-there is no discount to apply
+  # CashRegister #apply_discount the cash register was not initialized with an employee discount returns a string error message that there is no discount to apply
   def apply_discount
     discounted_total = @total * @employee_discount
   end
