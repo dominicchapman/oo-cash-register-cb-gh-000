9ba53524da9a416f14da65f7a9d566764004a1b4
Dominic Chapman <dominicchapman@me.com>
2017-10-10 16:35:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   # CashRegister #apply_discount the cash register was not initialized with an employee discount returns a string error message that there is no discount to apply
   def apply_discount
     discounted_total = @total - (@total * @employee_discount)
-    @total = discounted_total
+    @total = discounted_total.to_i
     "After the discount, the total comes to $#{discounted_total.to_i}."
   end
 
