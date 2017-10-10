12b57602511441e949cbdbe094ee444dd8228cea
Dominic Chapman <dominicchapman@me.com>
2017-10-10 16:08:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,9 +3,9 @@ class CashRegister
 
   # CashRegister ::new sets an instance variable @total on initialization to zero
   # CashRegister ::new optionally takes an employee discount on initialization
-  def initialize(options = {})
+  def initialize(employee_discount = 0)
     @total = 0
-    @employee_discount = options[:employee_discount] || 0
+    @employee_discount = employee_discount
   end
 
 end
